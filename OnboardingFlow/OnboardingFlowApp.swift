1c1649059d878018bdd35cdd3d10377d59a24657
Tony David <70279376+handleryouth@users.noreply.github.com>
2024-08-13 09:01:09 +0700

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Tony Gultom on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
